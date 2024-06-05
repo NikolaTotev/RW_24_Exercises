3a079629414ffccd3a5e04c716d7a85a1efdf97f
NikolaTotevOcado <nikola.totev@ocado.com>
2024-06-05 12:48:55 +0300

    feat: Small fix for module 4

--- Solutions/Module_4_Servo_Control/Pico_Code/udp_servo_control.py
+++ Solutions/Module_4_Servo_Control/Pico_Code/udp_servo_control.py
@@ -18,6 +18,29 @@ data_lock = _thread.allocate_lock()
 
 udp_data = 0
 
+
+def angle_to_duty(target_angle):
+    print(f"target{target_angle}")
+    return int(6553/180 * target_angle + 1638)
+
+def easeInOutQuadratic(x: float):
+  return  (2 * pow(x, 2)) if (x < 0.5) else ((-2 * pow(x, 2)) + (4 * x) - 1)
+
+
+def easeToPosition (servo: machine.PWM, current, target, speed=3):    
+    print(f"Current {current}, Target {target}")
+    if current < target:
+        diff = target - current
+        for x in range (0, 1000, 6):
+            servo.duty_u16(current + int(easeInOutQuadratic(x/1000)*diff))
+            utime.sleep_ms(speed)
+    
+    if current > target:
+        diff = current - target
+        for x in range (0, 1000, 6):
+            servo.duty_u16(current - int(easeInOutQuadratic(x/1000)*diff))
+            utime.sleep_ms(speed)
+
 def inet_aton(addr:str):
     return bytes(map(int, addr.split(".")))
 
